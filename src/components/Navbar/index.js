fcc114b72cfe773c01c624d225274d813431567e
Mohammadreza Amini <aminiamini433@yahoo.fr>
2022-02-02 18:33:11 +0330

    Navbar Design

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa'
+import {Nav, NavbarContainer, NavLogo, MobileIcon, NavItems
+    , NavLinks, NavMenu, NavButton, NavButtonLink} from './NavbarElements'
+
+const Navbar = () => {
+  return (
+    <>
+        <Nav>
+            <NavbarContainer>
+                <NavLogo to='/'>dollar</NavLogo>
+                <MobileIcon>
+                    <FaBars />
+                </MobileIcon>
+                <NavMenu>
+                    <NavItems>
+                        <NavLinks to="about">About</NavLinks>
+                    </NavItems>
+                    <NavItems>
+                        <NavLinks to="discovery">Discovery</NavLinks>
+                    </NavItems>
+                    <NavItems>
+                        <NavLinks to="services">Services</NavLinks>
+                    </NavItems>
+                    <NavItems>
+                        <NavLinks to="signup">Sign Up</NavLinks>
+                    </NavItems>
+                </NavMenu>
+                <NavButton>
+                    <NavButtonLink to="/signin">Sign In</NavButtonLink>
+                </NavButton>
+            </NavbarContainer>
+        </Nav>
+    </>
+  );
+};
+
+export default Navbar;
