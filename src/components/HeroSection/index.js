982d993d79e8c9844e4b440b48c1b41619349bcf
Mohammadreza Amini <aminiamini433@yahoo.fr>
2022-02-02 21:31:15 +0330

    Navbar update

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const Hero = () => {
   return (
-      
+      <HeroContainer>
+          <HeroBackground>
+              <VideoBackground autoPlay loop muted src = {video} type='video/mp4' />
+          </HeroBackground>
+      </HeroContainer>
   )
 };
 
