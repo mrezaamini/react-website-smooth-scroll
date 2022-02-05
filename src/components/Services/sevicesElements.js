66a5e7d73c3c46cf290d1f838df1cc65f2fde8fa
Mohammadreza Amini <aminiamini433@yahoo.fr>
2022-02-05 14:32:43 +0330

    ServicesUpdate

--- src/components/Services/sevicesElements.js
+++ src/components/Services/sevicesElements.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ServicesContainer = styled.div`
-    height: 900px;
+    height: 860px;
     display: flex;
     flex-direction: column;
     justify-content: center;
