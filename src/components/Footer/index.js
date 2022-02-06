b88f1aa48c7eec386e6001ff7d468ddf9822dc18
Mohammadreza Amini <aminiamini433@yahoo.fr>
2022-02-06 11:53:26 +0330

    FooterUpdate

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,97 @@
-import React from 'react';
+import React from "react";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaYoutube,
+  FaTwitter,
+} from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterWrapper,
+  FooterLinksContainer,
+  FooterLinkWrapper,
+  FooterLinkItem,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrapper,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+} from "./FooterElements";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <FooterContainer>
+      <FooterWrapper>
+        <FooterLinksContainer>
+          <FooterLinkWrapper>
+            <FooterLinkItem>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to="/">How it works</FooterLink>
+              <FooterLink to="/">Testimonials</FooterLink>
+              <FooterLink to="/">Careers</FooterLink>
+              <FooterLink to="/">Investors</FooterLink>
+              <FooterLink to="/">Terms of service</FooterLink>
+            </FooterLinkItem>
+
+            <FooterLinkItem>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to="/">Contact</FooterLink>
+              <FooterLink to="/">Support</FooterLink>
+              <FooterLink to="/">Destinations</FooterLink>
+              <FooterLink to="/">Sponserships</FooterLink>
+            </FooterLinkItem>
+          </FooterLinkWrapper>
+
+          <FooterLinkWrapper>
+            <FooterLinkItem>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/">Submit video</FooterLink>
+              <FooterLink to="/">Ambassadors</FooterLink>
+              <FooterLink to="/">Agency</FooterLink>
+              <FooterLink to="/">Influencer</FooterLink>
+            </FooterLinkItem>
+
+            <FooterLinkItem>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/">Instagram</FooterLink>
+              <FooterLink to="/">Facebook</FooterLink>
+              <FooterLink to="/">Youtube</FooterLink>
+              <FooterLink to="/">Twitter</FooterLink>
+            </FooterLinkItem>
+          </FooterLinkWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrapper>
+            <SocialLogo to="/">BCITY</SocialLogo>
+            <WebsiteRights>
+              BCITY © {new Date().getFullYear()} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Youtube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrapper>
+        </SocialMedia>
+      </FooterWrapper>
+    </FooterContainer>
+  );
 };
 
 export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,34 @@
-import React, {useState} from 'react';
-import Sidebar from '../components/Sidebar'
-import Navbar from '../components/Navbar'
-import HeroSection from '../components/HeroSection'
-import Services from '../components/Services';
-import InfoSection from '../components/infoSection';
-import { homeObjectOne, homeObjectTwo, homeObjectThree } from '../components/infoSection/data';
+import React, { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import Navbar from "../components/Navbar";
+import HeroSection from "../components/HeroSection";
+import Services from "../components/Services";
+import InfoSection from "../components/infoSection";
+import {
+  homeObjectOne,
+  homeObjectTwo,
+  homeObjectThree,
+} from "../components/infoSection/data";
+import Footer from "../components/Footer";
 
 const Home = () => {
-
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
   const toggle = () => {
-    setIsOpen(!isOpen)
-  }
-  
+    setIsOpen(!isOpen);
+  };
+
   return (
-      <>
+    <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
       <HeroSection />
-      <InfoSection {...homeObjectOne}/>
-      <InfoSection {...homeObjectTwo}/>
+      <InfoSection {...homeObjectOne} />
+      <InfoSection {...homeObjectTwo} />
       <Services />
-      <InfoSection {...homeObjectThree}/>
-      
-      </>
-  )
+      <InfoSection {...homeObjectThree} />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
